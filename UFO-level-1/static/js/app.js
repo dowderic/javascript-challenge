c1cbf8147564c5b41ef4e0421f0bfd1be8f4a7f0
Eric Dowd <dowderic@hotmail.com>
2021-02-24 21:39:42 -0500

    .

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,3 +9,13 @@ var ttable = d3.select("table");
 
 // confirming data with a console.log
 console.log(data);
+
+// using an arrow function to populate the imbeded table
+data.forEach((ufoSighting) => {
+  var row = ttable.append("tr");
+  Object.entries(ufoSighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
